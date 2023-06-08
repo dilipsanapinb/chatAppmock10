7309e081677268fbf63f7e7adb17d123bb9e9fc3
dilipsanapinb <dilipinbsanap@gmail.com>
2023-06-08 13:41:13 +0530

    group chat app

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send("Welcome to Chat App")
+})
+
 app.use("/user", userRoute);
 
 app.listen(process.env.port, async () => {
@@ -29,11 +33,11 @@ app.listen(process.env.port, async () => {
   console.log(`Server is running on port ${process.env.port}`);
 });
 
-app.use(
-  express.static(
-    path.join(__dirname, "https://mockchatapp.onrender.com/Frontend")
-  )
-);
+// app.use(
+//   express.static(
+//     path.join(__dirname, "https://mockchatapp.onrender.com/Frontend")
+//   )
+// );
 
 const io = new Server(server);
 
